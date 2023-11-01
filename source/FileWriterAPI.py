84d178a29efdd3acce18d139d44780e6f29a1a35
Tatiana Person <tatyperson22@gmail.com>
2023-11-01 15:55:50 +0100

    Updating FileWriterAPI interface

--- source/FileWriterAPI.py
+++ source/FileWriterAPI.py
@@ -4,6 +4,6 @@ class FileWriterAPI:
 		"""Create the file to save the results of the analysis in the selected format."""
 	pass
 
-	def writeLine(filePath, fileIndex, lineFields, lineIndex):
+	def writeLine(filePath, lineContent):
 		"""Write the fields in a new row to save the results of the analysis in the selected file and the selected format."""
 	pass
